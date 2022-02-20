6a04d05cb2229efb6da828fc233e0027d7491733
gilver <danyagrachev@gmail.com>
2022-02-20 11:44:49 -0800

    init

---
+++ front/src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className='header__container'>
+          placeholder
+           </div>
+    </header>
+  );
+}
+
+export default Header;
