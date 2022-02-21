a623227f17fa6d00252bd6ce19c83f98ee5d3a99
gilver <danyagrachev@gmail.com>
2022-02-21 01:30:30 -0800

    db created

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose  from 'mongoose'
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  let a = await mongoose.connect('mongodb://mongo:27017')
+  await app.listen(5000);
 }
 bootstrap();
