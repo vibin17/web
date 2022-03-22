a6044614715eef152262090ddecdd2b8fa89346f
Данила Грачев <danyagrachev@gmail.com>
2022-03-22 09:39:43 +0600

    back file system works

--- back/shop-api/src/products/products.module.ts
+++ back/shop-api/src/products/products.module.ts
@@ -7,9 +7,18 @@ import { FilesModule } from 'src/files/files.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product.schema';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath: ENV_PATH}), FilesModule, MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}]),
+  imports: [ConfigModule.forRoot({ envFilePath: ENV_PATH }), 
+    FilesModule, 
+    MongooseModule.forFeature([{
+      name: Product.name, 
+      schema: ProductSchema
+    }]),
+    // ServeStaticModule.forRoot({
+    //   rootPath: path.resolve( __dirname, 'static'),
+    // }),
     ClientsModule.register([{
       name: 'AUTH_CLIENT',
       transport: Transport.TCP,
